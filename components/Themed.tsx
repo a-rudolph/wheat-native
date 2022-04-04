14e05c9661d0600c1f788eefe1126609b18bd022
a-rudolph <adam.rudolph@live.ca>
2022-04-03 23:23:51 -0400

    btr selects

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -3,10 +3,10 @@
  * https://docs.expo.io/guides/color-schemes/
  */
 
-import { Text as DefaultText, View as DefaultView } from 'react-native';
+import { Text as DefaultText, View as DefaultView } from "react-native";
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
@@ -25,31 +25,51 @@ export function useThemeColor(
 type ThemeProps = {
   lightColor?: string;
   darkColor?: string;
+  color?: keyof typeof Colors["dark"];
 };
 
-export type TextProps = ThemeProps & DefaultText['props'];
-export type ViewProps = ThemeProps & DefaultView['props'];
+export type TextProps = ThemeProps & DefaultText["props"];
+export type ViewProps = ThemeProps & DefaultView["props"];
 
 export function Text(props: TextProps) {
-  const { style, lightColor, darkColor, ...otherProps } = props;
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+  const { style, lightColor, darkColor, color = "text", ...otherProps } = props;
+  const textColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    color
+  );
 
-  return <DefaultText style={[{ color }, style]} {...otherProps} />;
+  return <DefaultText style={[{ color: textColor }, style]} {...otherProps} />;
 }
-
 
 export function Card(props: ViewProps) {
   const { style, ...otherProps } = props;
-  const scheme = useColorScheme()
-  
-  const backgroundColor = Colors[scheme].gradient
+  const scheme = useColorScheme();
 
-  return <DefaultView style={[{ backgroundColor, padding: 16, borderRadius: 8, borderWidth: 1, borderColor: 'rgba(72, 79, 85, 0.2)' }, style]} {...otherProps} />;
+  const backgroundColor = Colors[scheme].gradient;
+
+  return (
+    <DefaultView
+      style={[
+        {
+          backgroundColor,
+          padding: 16,
+          borderRadius: 8,
+          borderWidth: 1,
+          borderColor: "rgba(72, 79, 85, 0.2)",
+        },
+        style,
+      ]}
+      {...otherProps}
+    />
+  );
 }
 
 export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "background"
+  );
 
   return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,91 @@
-import { StyleSheet } from 'react-native';
+import { loadOptions } from "@babel/core";
+import { useMemo } from "react";
+import { useState } from "react";
+import { Pressable, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Card, Text, View } from "../components/Themed";
+import Colors from "../constants/Colors";
+import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+enum Btr {
+  OVERNIGHT = "overnight",
+  SAMEDAYER = "samedayer",
+}
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const [selectedBtr, setBtr] = useState<Btr>(Btr.SAMEDAYER);
+
+  const options = [
+    {
+      title: "sameday",
+      btr: Btr.SAMEDAYER,
+      desc: "mix in the morning, bake in the evening",
+    },
+    {
+      title: "overnight",
+      btr: Btr.OVERNIGHT,
+      desc: "mix in the afternoon, bake in the morning",
+    },
+  ];
+
+  const desc = useMemo(() => {
+    return options.find((option) => option.btr === selectedBtr)?.desc;
+  }, [selectedBtr]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.row}>
+        {options.map(({ title, btr }) => {
+          const isSelected = btr === selectedBtr;
+          const cardStyle = isSelected
+            ? {
+                borderColor: "#F6BB6366",
+              }
+            : {};
+
+          /**
+           * border: 1px solid #F6BB6366
+           */
+          return (
+            <Pressable
+              key={btr}
+              style={styles.button}
+              onPress={() => setBtr(btr)}
+            >
+              <Card style={cardStyle}>
+                <Text
+                  style={{
+                    fontWeight: "600",
+                    fontSize: 16,
+                    textTransform: "uppercase",
+                    letterSpacing: 1,
+                  }}
+                >
+                  {title}
+                </Text>
+              </Card>
+            </Pressable>
+          );
+        })}
+      </View>
+      <View style={styles.row}>
+        {desc && (
+          <Text
+            style={{ textAlign: "left", flex: 1, marginLeft: 16 }}
+            color="text_2"
+          >
+            {desc}
+          </Text>
+        )}
+      </View>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
     </View>
   );
 }
@@ -17,16 +93,22 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    paddingTop: 16,
   },
+  row: {
+    padding: 8,
+    flexDirection: "row",
+    display: "flex",
+  },
+  button: { flex: 1, margin: 8 },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
